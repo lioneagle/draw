fabcb2653dfdaa101b3d3c698aac57b48ce1fbc0
lioneagle <leoeagle@163.com>
2017-07-13 01:05:57 +0800

    modify install.bat for building 32bit exe file

--- install.bat
+++ install.bat
@@ -7,5 +7,13 @@ goto end
 call env.bat
 gofmt -w src
 go install %1
+if %GOARCH% == amd64 goto build_32
+goto end
+:build_32
+set GOARCH=386
+go build -o=%132.exe %1
+copy %132.exe  .\bin\%132.exe
+del %132.exe
+set GOARCH=amd64
 :end
 echo finished
