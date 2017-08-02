55f795dba6ee6baf7c76b5691d9799e147e5a9b7
lioneagle <leoeagle@163.com>
2017-08-02 21:52:49 +0800

    modify install.bat

--- install.bat
+++ install.bat
@@ -7,34 +7,30 @@ goto end
 call env.bat
 gofmt -w src
 
+if "%2" == "" (
+	set output32=.\bin\%132.exe
+	set output64=.\bin\%164.exe
+) else (
+	set output32=.\bin\%232.exe
+	set output64=.\bin\%264.exe
+)
+
 if %GOARCH% == amd64 (
 	go install %1
 
 	if exist .\bin\%1.exe (
-		if "%2" == "" (
-			copy .\bin\%1.exe  .\bin\%164.exe
-		) else (
-			copy .\bin\%1.exe  .\bin\%264.exe
-		)
+		copy .\bin\%1.exe  %output64%
 		del .\bin\%1.exe
 		
 		set GOARCH=386
-		if "%2" == "" (
-			go build -o=.\bin\%132.exe %1
-		) else (
-			go build -o=.\bin\%232.exe %1
-		)
+		go build -o=%output32% %1
 		set GOARCH=amd64
 	)
 
 ) else (
 
 	go install %1
-	if "%2" == "" (
-		copy .\bin\%1.exe  .\bin\%132.exe
-	) else (
-		copy .\bin\%1.exe  .\bin\%232.exe
-	)
+	copy .\bin\%1.exe  %output32%
 	del .\bin\%1.exe
 
 )
